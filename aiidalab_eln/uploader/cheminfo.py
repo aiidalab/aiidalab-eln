de985e679b1e04c49efe5e901aced3c49f608853
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-05-06 20:45:08 +0000

    Merge cif.py and isotherm.py into cheminfo.py

--- aiidalab_eln/uploader/cheminfo.py
+++ aiidalab_eln/uploader/cheminfo.py
@@ -44,3 +44,22 @@ def upload_isotherm(
         source_info=source_info,
     )
 
+def upload_cif(
+    sample_manager,
+    cifdata,
+    uuid: str,
+    aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
+):
+
+    source_info = {
+        "uuid": uuid,
+        "url": aiidalab_instance,
+        "name": "Structure optimized using the XXX app on AiiDAlab",
+    }
+
+    sample_manager.put_spectrum(
+        spectrum_type="xray",
+        name=f"{uuid}.cif",
+        filecontent=cifdata._prepare_cif(),
+        source_info=source_info,
+    )
