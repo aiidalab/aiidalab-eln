03aae0e4d25fb02e44bbaa7987db1da6c393885e
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-05-06 22:30:21 +0000

    Allow specifying object filename and rename isotherm_dict to isotherm

--- aiidalab_eln/uploader/cheminfo.py
+++ aiidalab_eln/uploader/cheminfo.py
@@ -4,38 +4,38 @@ from cheminfopy import Sample
 
 def upload_isotherm(
     sample_manager,
-    isotherm_dict: dict,
+    isotherm,
     adsorptive: str,
     filename: str = None,
     aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
 ):
     source_info = {
-        "uuid": isotherm_dict.uuid,
+        "uuid": isotherm.uuid,
         "url": aiidalab_instance,
         "name": "Isotherm simulated using the isotherm app on AiiDAlab",
     }
     meta = {
         "adsorptive": adsorptive,
-        "temperature": isotherm_dict["temperature"],
+        "temperature": isotherm["temperature"],
         "method": "GCMC",
     }
     jcamp = from_dict(
         {
             "x": {
-                "data": isotherm_dict["isotherm"]["pressure"],
-                "unit": isotherm_dict["isotherm"]["pressure_unit"],
+                "data": isotherm["isotherm"]["pressure"],
+                "unit": isotherm["isotherm"]["pressure_unit"],
                 "type": "INDEPENDENT",
             },
             "y": {
-                "data": isotherm_dict["isotherm"]["loading_absolute_average"],
-                "unit": isotherm_dict["isotherm"]["loading_absolute_unit"],
+                "data": isotherm["isotherm"]["loading_absolute_average"],
+                "unit": isotherm["isotherm"]["loading_absolute_unit"],
                 "type": "DEPENDENT",
             },
         },
         data_type="Adsorption Isotherm",
         meta=meta,
     )
-    name = f"{isotherm_dict.uuid}.jcamp" if filename is None else f"{filename}.jcamp"
+    name = f"{isotherm.uuid}.jcamp" if filename is None else f"{filename}.jcamp"
     sample_manager.put_spectrum(
         spectrum_type="isotherm",
         name=name,
@@ -47,29 +47,30 @@ def upload_isotherm(
 def upload_cif(
     sample_manager,
     cifdata,
+    filename: str = None,
     aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
 ):
 
     source_info = {
-        "uuid": uuid,
+        "uuid": cifdata.uuid,
         "url": aiidalab_instance,
         "name": "Structure optimized using the XXX app on AiiDAlab",
     }
 
     sample_manager.put_spectrum(
         spectrum_type="xray",
-        name=f"{cifdata.uuid}.cif",
+        name=f"{cifdata.uuid}.cif" if filename is None else f"{filename}.cif",
         filecontent=cifdata._prepare_cif(),
         source_info=source_info,
     )
 
-def object_uploader(obj, eln_instance, sample_uuid, token):
+def object_uploader(obj, eln_instance, sample_uuid, token, filename):
     sample_manager = Sample(
         eln_instance,
         sample_uuid=sample_uuid,
         token=token,
     )
     if obj.node_type == "data.dict.Dict.":
-        upload_isotherm(sample_manager, isotherm_dict=obj, adsorptive="N2")
+        upload_isotherm(sample_manager, isotherm=obj, adsorptive="N2", filename=filename)
     elif obj.node_type == "data.cif.CifData.":
-        upload_cif(sample_manager, obj) 
+        upload_cif(sample_manager, obj, filename=filename) 
