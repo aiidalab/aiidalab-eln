ecba97771d8c2c93b080f0767185f4ece45ce1b0
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2021-05-20 22:38:32 +0200

    Starting to add schema mapping  (#1)
    * Add new dependencies pytojcamp and cheminfopy.
    * Add CIF import and export.
    * Add isotherm export: convert isotherm dict to jcamp and upload it
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>

---
+++ aiidalab_eln/exporter/cheminfo.py
@@ -0,0 +1,76 @@
+from pytojcamp import from_dict
+from cheminfopy import Sample
+
+
+def export_isotherm(
+    sample_manager,
+    isotherm,
+    adsorptive: str,
+    filename: str = None,
+    aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
+):
+    source_info = {
+        "uuid": isotherm.uuid,
+        "url": aiidalab_instance,
+        "name": "Isotherm simulated using the isotherm app on AiiDAlab",
+    }
+    meta = {
+        "adsorptive": adsorptive,
+        "temperature": isotherm["temperature"],
+        "method": "GCMC",
+    }
+    jcamp = from_dict(
+        {
+            "x": {
+                "data": isotherm["isotherm"]["pressure"],
+                "unit": isotherm["isotherm"]["pressure_unit"],
+                "type": "INDEPENDENT",
+            },
+            "y": {
+                "data": isotherm["isotherm"]["loading_absolute_average"],
+                "unit": isotherm["isotherm"]["loading_absolute_unit"],
+                "type": "DEPENDENT",
+            },
+        },
+        data_type="Adsorption Isotherm",
+        meta=meta,
+    )
+    name = f"{isotherm.uuid}.jcamp" if filename is None else f"{filename}.jcamp"
+    sample_manager.put_spectrum(
+        spectrum_type="isotherm",
+        name=name,
+        filecontent=jcamp,
+        metadata=meta,
+        source_info=source_info,
+    )
+
+def export_cif(
+    sample_manager,
+    cifdata,
+    filename: str = None,
+    aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
+):
+
+    source_info = {
+        "uuid": cifdata.uuid,
+        "url": aiidalab_instance,
+        "name": "Structure optimized using the XXX app on AiiDAlab",
+    }
+
+    sample_manager.put_spectrum(
+        spectrum_type="xray",
+        name=f"{cifdata.uuid}.cif" if filename is None else f"{filename}.cif",
+        filecontent=cifdata._prepare_cif(),
+        source_info=source_info,
+    )
+
+def object_exporter(obj, eln_instance, sample_uuid, token, filename):
+    sample_manager = Sample(
+        eln_instance,
+        sample_uuid=sample_uuid,
+        token=token,
+    )
+    if obj.node_type == "data.dict.Dict.":
+        export_isotherm(sample_manager, isotherm=obj, adsorptive="N2", filename=filename)
+    elif obj.node_type == "data.cif.CifData.":
+        export_cif(sample_manager, obj, filename=filename) 
---
+++ aiidalab_eln/importer/cheminfo.py
@@ -0,0 +1,22 @@
+from aiida.plugins import DataFactory
+from cheminfopy import Sample
+
+
+def import_cif(eln_instance, sample_uuid, token):
+    object_type = DataFactory("cif")
+    sample = Sample(
+        instance=eln_instance, sample_uuid=sample_uuid, token=token
+    )
+    content = sample.get_spectrum(spectrum_type=spectrum_type, name=file_name)
+    file = io.BytesIO(bytes(content, "utf8"))
+    node = object_type(file=file)
+
+    eln_info = {
+        "eln_instance": eln_instance,
+        "sample_uuid": sample_uuid,
+        "spectrum_type": spectrum_type,
+        "file_name": file_name,
+    }
+    node.set_extra("eln", eln_info)
+    node.store()
+    return node
