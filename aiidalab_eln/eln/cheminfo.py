ce1119e5a682b7bc096210750fb6eed0a80a3280
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-06-30 22:23:10 +0000

    Add token url.

--- aiidalab_eln/eln/cheminfo.py
+++ aiidalab_eln/eln/cheminfo.py
@@ -6,6 +6,7 @@ from IPython.display import clear_output
 
 class CheminfoElnConnector(ElnConnector):
     access_token = traitlets.Unicode()
+    token_url = traitlets.Unicode()
     
     def __init__(self, **kwargs):
 
@@ -17,6 +18,7 @@ class CheminfoElnConnector(ElnConnector):
             style={"description_width": "initial"},
         )
         traitlets.link((self, "eln_instance"), (eln_instance_widget, "value"))
+        
         token_widget = ipw.Text(
             description="Token:",
             value="",
@@ -24,11 +26,19 @@ class CheminfoElnConnector(ElnConnector):
             style={"description_width": "initial"},
         )
         traitlets.link((self, "access_token"), (token_widget, "value"))
+        
+        token_url_widget = ipw.Text(
+            description="Token URL:",
+            value="",
+            style={"description_width": "initial"},
+        )
+        traitlets.link((self, "token_url"), (token_url_widget, "value"))        
+        
         self.button_clicked = True
         request_token_button = ipw.Button(description="Request token")
         request_token_button.on_click(self.request_token)
         self.output = ipw.Output() 
-        super().__init__(children=[eln_instance_widget, token_widget, request_token_button, self.output], **kwargs)
+        super().__init__(children=[eln_instance_widget, token_widget, token_url_widget, request_token_button, self.output], **kwargs)
         
     def connect(self):
         """Connect to the cheminfo ELN."""
@@ -38,17 +48,18 @@ class CheminfoElnConnector(ElnConnector):
         return {
             "eln_instance": self.eln_instance,
             "eln_type": self.eln_type,
-            "access_token": self.access_token
+            "access_token": self.access_token,
+            "token_url": self.token_url,
         }
     
     def request_token(self, _=None):
         with self.output:
             clear_output()
             if self.button_clicked:
-                display(ipw.HTML("""
+                display(ipw.HTML(f"""
                 Once it appears, copy the text from the frame below, and insert it to the "Token" field above.
                 <br/>
-                <iframe src="https://www.cheminfo.org/flavor/tools/Token/index.html" width="400" height="100"></iframe>
+                <iframe src="{self.token_url}" width="400" height="100"></iframe>
                 """))
         self.button_clicked = not self.button_clicked
     
