d00dd5e79fe786b271163a36c999916049721a30
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-07-09 17:33:47 +0200

    chore: add pre-commit

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
 import json
-from io import open
-from setuptools import setup, find_packages
 
-with open('setup.json', 'r', encoding='utf-8') as info:
+from setuptools import find_packages, setup
+
+with open("setup.json", "r", encoding="utf-8") as info:
     kwargs = json.load(info)
-setup(include_package_data=True,
-      packages=find_packages(),
-      long_description=open('README.md', encoding='utf-8').read(),
-      long_description_content_type='text/markdown',
-      **kwargs)
-
+setup(
+    include_package_data=True,
+    packages=find_packages(),
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    **kwargs
+)
