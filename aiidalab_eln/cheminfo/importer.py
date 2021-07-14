7a29bedbe9e938f65773536408b32a20d4238e86
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-07-14 16:56:10 +0000

    Improve import functionality of the ELN connector

--- aiidalab_eln/cheminfo/importer.py
+++ aiidalab_eln/cheminfo/importer.py
@@ -1,6 +1,24 @@
-# -*- coding: utf-8 -*-
 """Module to define import tools from Cheminfo ELN to AiiDAlab."""
 
+import io
 
-def import_cif():
+from aiida.plugins import DataFactory
+from ase.io import read
+
+
+def import_cif(sample, **kwargs):
     """Import CIF object from Cheminfo ELN to AiiDAlab."""
+    object_type = DataFactory("cif")
+    content = sample.get_spectrum(spectrum_type="xray", name=kwargs["file_name"])
+    file = io.BytesIO(bytes(content, "utf8"))
+    node = object_type(file=file)
+    return node
+
+
+def import_pdb(sample, **kwargs):
+    """Import PDB object from Cheminfo ELN to AiiDAlab."""
+    object_type = DataFactory("structure")
+    content = sample.get_spectrum(spectrum_type="xray", name=kwargs["file_name"])
+    file = io.BytesIO(bytes(content, "utf8"))
+    node = object_type(from_ase=read(file))
+    return node
