d68bf5e0e26bbdbda8ad18b80ef74979fcc893df
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2021-07-19 13:29:32 +0000

    chore: polish and housekeeping (#12)
    - chore: polish
    - close #9, close #10, close #11
    - add publish workflow
    - chore: close #15
    - fix: cheminfopy equal 0.4 also allowed

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ import json
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as readme_handle:
+    long_description = readme_handle.read()
+
 with open("setup.json", "r", encoding="utf-8") as info:
     kwargs = json.load(info)
 setup(
     include_package_data=True,
     packages=find_packages(),
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     **kwargs
 )
