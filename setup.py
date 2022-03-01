227f96e57c987cfe3c08a132cd9add8462eb5f0b
Carl Simon Adorf <carl.simon.adorf@gmail.com>
2022-03-01 13:45:42 +0100

    Use setup cfg and update the pre-commit step of CI (#36).

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-"""Describe the module distribution to the Distutils."""
-import json
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-with open("README.md") as readme_handle:
-    long_description = readme_handle.read()
-
-with open("setup.json", encoding="utf-8") as info:
-    kwargs = json.load(info)
-setup(
-    include_package_data=True,
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    **kwargs
-)
+setup()
