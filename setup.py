cfe4342a47644fc01de3945ca24daa707567a7eb
Carl Simon Adorf <carl.simon.adorf@gmail.com>
2022-03-01 10:36:41 +0100

    CI: Update pre-commit configuration. (#35)
    To be aligned with those of other packages within the aiidalab
    organization.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 """Describe the module distribution to the Distutils."""
 import json
 
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as readme_handle:
+with open("README.md") as readme_handle:
     long_description = readme_handle.read()
 
-with open("setup.json", "r", encoding="utf-8") as info:
+with open("setup.json", encoding="utf-8") as info:
     kwargs = json.load(info)
 setup(
     include_package_data=True,
